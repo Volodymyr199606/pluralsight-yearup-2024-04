ec2c380aee1ef6e100990308077dbe95685d54c6
unknown <vova.behma@gmail.com>
2024-04-10 17:22:28 -0700

    Exercises of declaring variables

--- workbook-1/DeclaringVariables/src/main/java/com/pluralsight/Main.java
+++ workbook-1/DeclaringVariables/src/main/java/com/pluralsight/Main.java
@@ -8,32 +8,67 @@ public class Main
 
         // a vehicle identification number in the range 1000000 - 9999999
 
+        float vIdNumber = 11000000 - 9999999;
+        System.out.println("Identification number is: " + vIdNumber);
+
 
         // a vehicle make /model (i.e. Ford Explorer)
 
+        String vehicle_model = "Ford Explorer";
+        System.out.println("Vehicle model is: " + vehicle_model);
+        
 
         // a vehicle color
+
+        String vehicleColor = "Blue";
+        System.out.println("The color is: " + vehicleColor);
 
 
         // whether the vehicle has a towing package
 
+        boolean vTowingPackage = true;
+        System.out.println("The veichle has towing package: " + vTowingPackage);
+
 
         // an odometer reading
+
+        double odReading = 1757.55;
+        System.out.println("Odometr reading is: " + odReading);
+
 
 
         // a price
 
+        double price  = 14.000;
+        System.out.println("The price is: " +price );
+
 
         // a quality rating (A, B, or C)
 
+        char qRating = 'B';
+        System.out.println("Vehicle Quality Rating: " + qRating);
+
 
         // a phone number
+
+        String phoneNumber = "510761939";
+        System.out.println("Phone Number is: " + phoneNumber);
+
 
 
         // a social security number
 
 
+        int socialSecurityNumber = 108274577 ;
+        System.out.println("Social Security Number is: " + socialSecurityNumber);
+
+
+
         // a zip code
+
+        int zCode = 94546;
+        System.out.println("Zip Code is: " + zCode);
+
 
 
 
--- workbook-1/demos/03-IntroToJava/src/main/java/com/pluralsight/Main.java
+++ workbook-1/demos/03-IntroToJava/src/main/java/com/pluralsight/Main.java
@@ -1,9 +1,42 @@
 package com.pluralsight;
 
+import javax.print.DocFlavor;
+
 public class Main
 {
     public static void main(String[] args)
     {
         System.out.println("Hello world!");
+
+
+
+        // numbers//
+
+        // declare a variable that has the count of kids on a bus//
+
+
+        // declare a variable that has a count of the number of buses in the school district
+
+
+        // price of chocolate//
+
+        float priceOfChocolate =  1.5f;
+
+        // are kids on the bus?
+        boolean kidsOnBus = true;
+
+        // the first letter of the alphabet
+        char firstLetter = 'a';
+
+        // name of your favorite restaraunt
+        // string is not a primitive
+        String favoriteRestaraunt = "Texas Roadhose ";
+
+
+
+
+
     }
 }
+
+
