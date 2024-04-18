ab55519820333f634005df85c9c4468b5f444e04
Volodymyr <vova.behma@gmail.com>
2024-04-17 18:09:29 -0700

    CelPhone Exercise 1

--- workbook-2/demos/03-IntroToClasses-review-starter/src/main/java/com/pluralsight/EventApplication.java
+++ workbook-2/demos/03-IntroToClasses-review-starter/src/main/java/com/pluralsight/EventApplication.java
@@ -11,7 +11,6 @@ public class EventApplication
     public static void main(String[] args)
     {
 
-
     }
 
     public static void register()
---
+++ workbook-2/demos/CellPhoneService/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,63 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+        Scanner userInput = new Scanner(System.in);
+
+        // instance
+        CelPhone myPhone = new CelPhone();
+
+
+
+        // userPrompt
+
+        System.out.println("Please enter your information below: ");
+        System.out.print("What is Serial number: ? ");
+        int serialNumber = userInput.nextInt();
+
+        userInput.nextLine();
+
+        System.out.print("What model is the phone: ? ");
+        String model = userInput.nextLine();
+
+        System.out.print("Who is the Carrier: ? ");
+        String carrier = userInput.nextLine();
+
+        System.out.print("What is the Phone Number: ? ");
+        String phoneNumber = userInput.nextLine();
+
+        System.out.print("Device owner: ?  ");
+        String owner = userInput.nextLine();
+
+
+
+        // setters
+
+        myPhone.setSerialNumber(serialNumber);
+        myPhone.setModel(model);
+        myPhone.setCarrier(carrier);
+        myPhone.setPhoneNumber(phoneNumber);
+        myPhone.setOwner(owner);
+
+        // display
+
+        System.out.println("\nPhone Information: ");
+        System.out.println("Serial Number: " + serialNumber);
+        System.out.println("Model: " + model);
+        System.out.println("Carrier: " + carrier);
+        System.out.println("PhoneNumber: "+ phoneNumber);
+        System.out.println("Owner: " + owner);
+
+
+    }
+
+}
+
+
+
+
