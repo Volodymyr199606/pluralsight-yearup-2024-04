c77f476860cf3dad27f68db7163bd77b3d3fed07
Volodymyr199606 <166452639+Volodymyr199606@users.noreply.github.com>
2024-05-06 15:01:40 -0700

    small changes

--- workbook-3/demos/05-Collections-starter/src/main/java/com/pluralsight/Main.java
+++ workbook-3/demos/05-Collections-starter/src/main/java/com/pluralsight/Main.java
@@ -1,11 +1,18 @@
-package com.pluralsight;
 
-import com.pluralsight.io.Logger;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Main
 {
-    public static void main(String[] args)
-    {
 
+    public static void main(String[] args) {
+        displayDateTime();
+    }
+
+    public static void displayDateTime() {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formattedDateTime = now.format(formatter);
+        System.out.println(formattedDateTime);
     }
 }
