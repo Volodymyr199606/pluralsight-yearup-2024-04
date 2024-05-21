a4ac691e3f860ff4496956bb7d2ea2f5907190be
Volodymyr199606 <166452639+Volodymyr199606@users.noreply.github.com>
2024-05-21 15:52:17 -0700

    exercise 2 that defines a class named Person done

---
+++ workbook-6/demos/WorkingWithInterfaces/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,21 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Person> myFamily = new ArrayList<>();
+        myFamily.add(new Person("Dana", "Wyatt", 63));
+        myFamily.add(new Person("Zachary", "Westly", 31));
+        myFamily.add(new Person("Elisha", "Aslan", 14));
+        myFamily.add(new Person("Ian", "Auston", 16));
+
+        Collections.sort(myFamily);
+
+        for (Person person : myFamily) {
+            System.out.println(person);
+        }
+    }
+}
