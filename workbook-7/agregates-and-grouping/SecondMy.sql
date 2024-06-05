1981def3eeaf0816abc9fbbce8144a97854f04b4
Volodymyr199606 <166452639+Volodymyr199606@users.noreply.github.com>
2024-06-05 15:01:19 -0700

    start working on another exercise, not finished yet....

---
+++ workbook-7/agregates-and-grouping/SecondMy.sql
@@ -0,0 +1,47 @@
+use northwind;
+
+/*SELECT COUNT(*) FROM Suppliers;*/
+
+
+/*SELECT SUM(salary) FROM Employees;*/
+
+
+/*SELECT MIN(UnitPrice) FROM Products;*/
+
+
+
+/*SELECT AVG(UnitPrice) FROM Products;*/
+
+
+
+/*SELECT MAX(UnitPrice) FROM Products;*/
+
+
+
+
+/*DESCRIBE products;
+SELECT SupplierID, COUNT(ProductID)
+FROM Products
+GROUP BY SupplierID;*/
+
+
+
+
+
+/*SELECT SupplierID, CategoryID, AVG(UnitPrice) 
+FROM Products 
+GROUP BY SupplierID, CategoryID;*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
